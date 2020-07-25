6ae3270c460c5347930c823d7f4512fd9f13bd5a
hasan doha <doha.physics.du@gmail.com>
2020-07-25 08:57:41 -0500

    RAT bash file is updated...

--- RAT.sh
+++ RAT.sh
@@ -3,7 +3,12 @@
 cd Layers
 
 
-python3 Air_hBN_BP_Au_SiO2_Si_thickness_image.py
+python3 Air_hBN_BP_SiO2_Si_thickness_image.py
+# python3 Air_hBN_BP_SiO2_Si_absorbance.py
+
+# python3 RAT2igorPro_Air_hBN_BP_SiO2_Si_thickness_image.py
+
+# python3 Air_hBN_BP_Au_SiO2_Si_thickness_image.py
 # python3 Air_hBN_BP_Au_SiO2_Si_absorbance.py
 
 
